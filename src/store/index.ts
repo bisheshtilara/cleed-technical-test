534194f85fca8d53b08839024a2ae9ba728b43f3
bisheshtilara <90610832+bisheshtilara@users.noreply.github.com>
2024-06-19 12:08:30 +0200

    feat/vuex (#2)
    * chore: added vuex with type declaration
    
    * feat: added interface for todo
    
    * feat: setup vuex store for todo
    
    * chore: added vuex-persist
    
    * feat: setup vuex-persist to persist the data to local storage

---
+++ src/store/index.ts
@@ -0,0 +1,52 @@
+import { type ITodo } from "@/types/todo"
+import { createStore } from "vuex"
+import VuexPersistence from "vuex-persist"
+
+interface IState {
+  todos: ITodo[]
+  nextTodoId: number
+}
+
+const vuexLocal = new VuexPersistence<IState>({
+  storage: window.localStorage,
+})
+
+export default createStore<IState>({
+  state: {
+    todos: [],
+    nextTodoId: 1,
+  },
+  plugins: [vuexLocal.plugin],
+  getters: {
+    allTodos: (state) => state.todos,
+  },
+  mutations: {
+    ADD_TODO: (state, todo: Omit<ITodo, "id" | "completed">) => {
+      const newTodo: ITodo = {
+        ...todo,
+        id: state.nextTodoId,
+        completed: false,
+      }
+      state.todos.push(newTodo)
+      state.nextTodoId++
+    },
+    TOGGLE_TODO: (state, todoId: number) => {
+      const todo = state.todos.find((todo) => todo.id === todoId)
+      if (todo) todo.completed = !todo.completed
+    },
+    DELETE_TODO: (state, todoId: number) => {
+      state.todos = state.todos.filter((todo) => todo.id !== todoId)
+    },
+  },
+  actions: {
+    addTodo: ({ commit }, todo: Omit<ITodo, "id" | "completed">) => {
+      commit("ADD_TODO", todo)
+    },
+    toggleTodo: ({ commit }, todoId: number) => {
+      commit("TOGGLE_TODO", todoId)
+    },
+    deleteTodo: ({ commit }, todoId: number) => {
+      commit("DELETE_TODO", todoId)
+    },
+  },
+})
