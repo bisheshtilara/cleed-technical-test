e95589b397f33aea1cfafe620d9ac7036bcffcdd
Bishesh Tilara <bisheshtilara@gmail.com>
2024-06-19 15:50:40 +0200

    feat: add Toaster component for displaying notifications

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import ModeToggle from "@/components/ModeToggle.vue"
+import Toaster from "@/components/ui/toast/Toaster.vue"
 </script>
 <template>
   <main class="w-screen h-screen">
+    <Toaster />
     <div class="absolute top-1 right-1 md:top-4 md:right-4">
       <ModeToggle />
     </div>
