a98f97bde3c42e5d2d19cf369dbf8f7c940b1929
Bishesh Tilara <bisheshtilara@gmail.com>
2024-06-19 15:50:46 +0200

    feat: add toast notifications for form validation and action success

--- src/pages/AddTodo.vue
+++ src/pages/AddTodo.vue
@@ -12,13 +12,15 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { router } from "@/router"
-import { CircleChevronLeft } from "lucide-vue-next"
+import { BadgePlus, CircleChevronLeft, Pencil } from "lucide-vue-next"
 import { onMounted, ref } from "vue"
 import { useRoute } from "vue-router"
 import { useStore } from "vuex"
+import { useToast } from "@/components/ui/toast/use-toast"
 
 const route = useRoute()
 const store = useStore()
+const { toast } = useToast()
 
 onMounted(() => {
   if (!store.getters["todoExists"](Number(route.query.id))) {
@@ -49,8 +51,20 @@ const editTodo = () => {
 }
 
 const todoAction = () => {
-  if (title.value.trim() === "" || description.value.trim() === "") return
+  if (title.value.trim() === "") {
+    toast({
+      title: "Incomplete info",
+      description: "Please fill in atleast the title",
+    })
+    return
+  }
   isEdit.value ? editTodo() : addTodo()
+  toast({
+    description: isEdit.value
+      ? "Todo updated successfully"
+      : "Todo added successfully",
+  })
+  title.value = ""
   description.value = ""
   router.push("/")
 }
@@ -98,9 +112,18 @@ const todoAction = () => {
         </div>
       </CardContent>
       <CardFooter>
-        <Button @click="todoAction">
-          {{ isEdit ? "Edit Todo" : "Add Todo" }}
-        </Button>
+        <div class="flex justify-end w-full">
+          <Button
+            @click="todoAction"
+            disabled="!title"
+            class="w-36 space-x-2"
+            size="sm"
+          >
+            <p>{{ isEdit ? "Edit Todo" : "Add Todo" }}</p>
+            <Pencil :size="16" v-if="isEdit" />
+            <BadgePlus v-else :size="20" />
+          </Button>
+        </div>
       </CardFooter>
     </Card>
   </div>
