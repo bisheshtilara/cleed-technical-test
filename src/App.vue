47137ab2bd2f323929dc7cdce41f2946309c21a8
Bishesh Tilara <bisheshtilara@gmail.com>
2024-06-19 11:07:23 +0200

    init: initialized vue3 vite project with tailwind, shadcn and vue router

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ModeToggle from "@/components/ModeToggle.vue"
+</script>
+<template>
+  <main class="w-screen h-screen">
+    <ModeToggle />
+    <RouterView />
+  </main>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <p>Home</p>
+  </div>
+</template>
